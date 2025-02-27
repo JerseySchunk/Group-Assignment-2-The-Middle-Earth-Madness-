854e4a31eb011a36aead583dab704ce883d82551
JerseySchunk <jjsprq@umsystem.edu>
2025-02-27 11:04:11 -0600

    Cleaned up comments & javadoc. Added getters and setters - Jersey

--- src/middleearth/Elf.java
+++ src/middleearth/Elf.java
@@ -1,18 +1,28 @@
 package middleearth;
 
-
-//The Elf class which inherits from MiddleEarthCharacter
+/**
+ * The Elf class that represents an Elf character.
+ * It extends MiddleEarthCharacter and determines Elf specific traits. 
+ */
 public class Elf extends MiddleEarthCharacter {
 	
-	//Constructor for creating an elf.
-	//Will call the parent class (MiddleEarthCharacter) constructor to initialize the fields. 
+	/**
+	 * Constructor for creating an elf.
+	 * Calls the parent class (MiddleEarthCharacter) constructor to intialize the fields.
+	 * @param name - Name of the Elf.
+	 * @param health - Initial health of the Elf.
+	 * @param power - Attack power of the elf.
+	 */
 	public Elf(String name, double health, double power) {
 		super(name, health, power);
 	}
-	
-	//Overriding the parent method of attack.
-	//This implements the attack behavior of the Elf race.
-	//They cannot attack other Elves or Dwarfs
+	/**
+	 * Overrides the parent method of attack from MiddleEarthCharacter
+	 * Implements the attack behavior of elves
+	 * Elves cannot attack their own kin. Elves or Dwarfs.
+	 * Elves deal 1.5x damage to Orcs.
+	 * Normal damage to other targets. 
+	 */
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
 		//Checking if the target is an Elf or Dwarf. If so, returns false.
@@ -20,6 +30,41 @@ public class Elf extends MiddleEarthCharacter {
 			System.out.println(name + "'s attack was ineffective! Cannot harm their own kin!");
 			return false;
 		}
+		
+		//Increases damage by 1.5 if target is Orc
+		double damage = power;
+		if(target instanceof Orc) {
+			damage = power * 1.5;
+		}
+		
+		//Apply damage to target
+		target.setHealth(target.getHealth() - damage);
+		System.out.println(name + " attacks " + target.getName() + " dealing " + damage + " damage!");
+		return true;
 	}
-
+	
+	
+	/** Getter method for Elf health.
+	 * @return The current health of the Elf character
+	 */
+	public double getHealth() {
+		return health;
+	}
+	
+	/**
+	 * Setter for the Elf health.
+	 * @param health - The new health value.
+	 */
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	
+	/**
+	 * Returns the race of the character.
+	 * @return "Elf"
+	 */
+	@Override
+	public String getRace() {
+		return "Elf";
+	}
 }
