75c1e041406c70625dd45e3b27793c2655008a5f
JerseySchunk <jjsprq@umsystem.edu>
2025-02-27 08:33:54 -0600

    Added instance variables and constructor - Jersey

--- src/middleearth/MiddleEarthCharacter.java
+++ src/middleearth/MiddleEarthCharacter.java
@@ -1,5 +1,15 @@
 package middleearth;
 
 public abstract class MiddleEarthCharacter {
-
+	//Protected variables to encourage inheritance to subclasses
+	protected String name;
+	protected double health;
+	protected double power;
+	
+	//Constructor to initialize common fields for the subclasses
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
 }
