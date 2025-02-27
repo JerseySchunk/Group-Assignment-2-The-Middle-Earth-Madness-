c9779065ef8d91ddebcaa721eaa834ffc8f333c9
JerseySchunk <jjsprq@umsystem.edu>
2025-02-27 09:02:42 -0600

    Added constructor for Elf - Jersey

--- src/middleearth/Elf.java
+++ src/middleearth/Elf.java
@@ -1,5 +1,11 @@
 package middleearth;
 
 public class Elf extends MiddleEarthCharacter {
+	
+	//Constructor for creating an elf.
+	//Will call the parent class (MiddleEarthCharacter) constructor to initialize the fields. 
+	public Elf(String name, double health, double power) {
+		super(name, health, power);
+	}
 
 }
