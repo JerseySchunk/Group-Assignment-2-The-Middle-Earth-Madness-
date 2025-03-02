be0ab01965454500783fe714e80ec72c86ef5603
Gideon <gideonmasters09876@gmail.com>
2025-03-02 12:50:05 -0800

    Generated Character Manager and added Comments to the Singleton class

--- src/middleearth/MiddleEarthCouncil.java
+++ src/middleearth/MiddleEarthCouncil.java
@@ -2,12 +2,19 @@ package middleearth;
 
 public class MiddleEarthCouncil {
 	
+	//The Singleton
 	private static MiddleEarthCouncil instance;
 	
+	//
+	private CharacterManager characterManager;
+	
+	// private to prevent making more then one instance of it
 	private MiddleEarthCouncil() {
+		this.characterManager = new CharacterManager();
 		
 	}
 	
+	//checks to see if a instance is already made if not makes a new one
 	public static MiddleEarthCouncil getInstance() {
 		if(instance == null) {
 			instance = new MiddleEarthCouncil();
@@ -15,9 +22,10 @@ public class MiddleEarthCouncil {
 		return instance;
 	}
 
-
-	public static void setInstance(MiddleEarthCouncil instance) {
-		MiddleEarthCouncil.instance = instance;
+	//allows access to CharacterManager
+	public CharacterManager getCharcterManager() {
+		return characterManager;
 	}
+
 
 }
