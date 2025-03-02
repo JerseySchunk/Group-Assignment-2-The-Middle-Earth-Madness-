dbfb10a407cf703334523fa7a6930ff08e56c110
Gideon <gideonmasters09876@gmail.com>
2025-03-02 12:34:39 -0800

    Working on the Singleton, not working yet

--- src/middleearth/MiddleEarthCouncil.java
+++ src/middleearth/MiddleEarthCouncil.java
@@ -1,5 +1,23 @@
 package middleearth;
 
 public class MiddleEarthCouncil {
+	
+	private static MiddleEarthCouncil instance;
+	
+	private MiddleEarthCouncil() {
+		
+	}
+	
+	public static MiddleEarthCouncil getInstance() {
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance;
+	}
+
+
+	public static void setInstance(MiddleEarthCouncil instance) {
+		MiddleEarthCouncil.instance = instance;
+	}
 
 }
