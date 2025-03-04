3fb7295404eb224e6b778fdcceba9c8e040bba6d
Son <Son@MSI>
2025-03-04 13:28:23 -0600

    This commit is to add the array and the fields that double the size when full and to keep track of the stored characters.

--- src/middleearth/CharacterManager.java
+++ src/middleearth/CharacterManager.java
@@ -1,5 +1,14 @@
 package middleearth;
-
 public class CharacterManager {
 
+	int ArraySize = 10;
+	private MiddleEarthCharacter[][] newMiddleEarthCharacter;
+	int size;
+	
+	MiddleEarthCharacter[] Characters = newMiddleEarthCharacter[ArraySize];
+	{if (Characters.length == ArraySize) {
+		ArraySize = ArraySize * 2;}
+	 
+	
+	}
 }
