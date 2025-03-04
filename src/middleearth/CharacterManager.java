77ed66e880d28c5d509cb115c4ba30c4da420500
Son <Son@MSI>
2025-03-04 15:40:33 -0600

    Adds add character method and array size function.

--- src/middleearth/CharacterManager.java
+++ src/middleearth/CharacterManager.java
@@ -10,6 +10,24 @@ public class CharacterManager {
         this.capacity = 9;  // Default capacity
         this.characters = new MiddleEarthCharacter[capacity]; // Create the array
         this.size = 0; // No characters initially
+
     }
+ // Adds character to the array
+    public boolean addCharacter(MiddleEarthCharacter c) {
+        if (size == capacity) {  // checks size against capacity 
+            capacity = capacity * 2;  // Double the capacity
+            MiddleEarthCharacter[] newCharacters = new MiddleEarthCharacter[capacity];  // Create a new larger array
+            for (int i = 0; i < size; i++) {
+                newCharacters[i] = characters[i];  // Copy characters to new array
+            }
+            characters = newCharacters;  // Makes old array point to new array
+        }
+        characters[size] = c;  // Add new character
+        size++;  // Increase size
+        return true;
+    }
+        
+        
+        
 
 }
